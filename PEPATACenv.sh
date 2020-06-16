d64d735020c33f950f9d59397f76b150d8766c1c
benpkeith <55356302+benpkeith@users.noreply.github.com>
2020-06-16 15:13:52 -0400

    Update PEPATACenv

--- PEPATACenv.sh
+++ PEPATACenv.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -i
 # Ben Keith
 # Script to load the various environmental variables for pepatac
-# USAGE: bash PEPATACenv.sh <genome>
+# USAGE: source PEPATACenv.sh <genome>
 
 if (($# == 0)); then
-  echo "NO INPUT PARAMETERS! Enter either "human" or "mouse" for the first\
-   argument"; exit;
+  echo "NO INPUT PARAMETERS! Enter either 'human' or 'mouse' for the first argument"
 fi
 
 
@@ -23,7 +22,7 @@ module load bedtools/2.29
 module load bowtie2/2.4.1
 module load fastqc/0.11.8
 module load samblaster/0.1.24
-module load samtools/1.9
+module load samtools/1.10
 module load skewer/0.2.2
 module load macs/2.2.7.1
 
@@ -34,13 +33,10 @@ module list
 #### ENV SETUP ####
 ###################
 
-# Furey lab bin
-export PATH='$PATH:/proj/fureylab/bin/'
-
-export PATH='$PATH:/path/pepatac/tools/'
-
+export PATH=/proj/fureylab/bin:$PATH
 
 ##### MIGHT BE BEST TO HARD CODE THESE INTO THE CONFIG FILES!
+export CODE='/proj/fureylab/pipelines/ATAC/pepatac_0.9.0'
 export CODEBASE='/proj/fureylab/pipelines/ATAC/pepatac_0.9.0/tools'
 export PEPENV='/proj/fureylab/pipelines/ATAC/pepatac_0.9.0/pipelines/pepatac.yaml'
 export DIVCFG='/proj/fureylab/pipelines/ATAC/pepatac_0.9.0/divcfg/unc_longleaf.yaml'
@@ -54,4 +50,7 @@ then
   export GENOMES='/proj/fureylab/genomes/mouse/refgenie/'
 fi
 
-echo "Environmental variables set. Exiting..."
+if (($# == 0)); then
+  echo "NO INPUT PARAMETERS! GENOME POINTERS NOT LOADED"
+  echo "Re-source this file after adding 'human' or 'mouse' argument"
+fi
