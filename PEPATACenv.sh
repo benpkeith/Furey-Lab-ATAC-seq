ccb5ca6f1790e9c53af96a7f4e82ad9ab691a0ad
benpkeith <55356302+benpkeith@users.noreply.github.com>
2020-06-18 09:56:44 -0400

    Update PEPATACenv.sh

--- PEPATACenv.sh
+++ PEPATACenv.sh
@@ -19,6 +19,7 @@ echo "LOADING MODULES"
 module load macs/2.2.7.1
 module load python/3.6.6
 module load r/3.6.0
+module load perl/5.18.2
 module load bedtools/2.29
 module load bowtie2/2.4.1
 module load fastqc/0.11.8
